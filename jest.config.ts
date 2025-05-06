3af5ad6ff375ec3471c856f517ff75e7104cca6c
WolvarineXD <148878451+WolvarineXD@users.noreply.github.com>
2025-05-06 17:22:35 +0530

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import { compilerOptions } from './tsconfig.json';
 
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // ✅ point to the setup file
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/?(*.)+(test).[tj]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
 
 export default config;
