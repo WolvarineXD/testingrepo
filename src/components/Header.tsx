eb7d24b4c92ed650ff0114e21ea25f9c79fdac3e
WolvarineXD <148878451+WolvarineXD@users.noreply.github.com>
2025-05-08 12:37:37 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="bg-theme-black py-4">
       <div className="container mx-auto px-6">
         <h1 className="text-3xl font-bold text-white">
-          <span className="text-theme-orange">Indian</span> Digital Vision
+          <span className="text-theme-orange">India</span> Digital Vision
         </h1>
       </div>
     </header>
